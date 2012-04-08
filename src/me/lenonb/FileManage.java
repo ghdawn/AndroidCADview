02e354e459464f16455009523be67d4cccc1f05a
ghdawn <ghdawn31@gmail.com>
2012-04-08 22:50:48 +0800

    修改了一些FileManage，删除了无用的文件

--- src/me/lenonb/FileManage.java
+++ src/me/lenonb/FileManage.java
@@ -7,60 +7,67 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.util.*;
 
-public class FileManage {
-	private IFileObtain obtain ;
-	private String lName[] ;
-	private String path ;
+public class FileManage
+{
+	private IFileObtain obtain;
+	private ArrayList<String> _fileName;
+	private String path;
 
-	public FileManage(IFileObtain obtain , String path) {
-		this.obtain = obtain ;
-		this.path = path ;
-		try {
-			obtain.Open(this.path , "list") ;
-			BufferedReader list = new BufferedReader(new InputStreamReader(obtain.getFile())) ;
-			String temp = " ";
-			ArrayList<String> nameList = null ;
-					
-			while(temp != null) {
-				temp = list.readLine() ;
-				nameList.add(temp) ;
-			}
-			lName = (String[])nameList.toArray() ;
-		}
-		catch(IOException e) {
-			System.out.println("error !") ;
-		}
-	}
-	
-	public String[] Search(String name) {
-		String[] str ;
-		int i = 0 ;
-		ArrayList<String> nameList = null ;
-		String temp = " " ;
-
-		while(temp != null) 
+	public FileManage(IFileObtain obtain, String path)
+	{
+		this.obtain = obtain;
+		this.path = path;
+		try
 		{
-			temp = lName[i] ;
-			if(temp.indexOf(name) != -1) {
-				nameList.add(temp) ;
+			obtain.Open(this.path, "list");
+			BufferedReader list = new BufferedReader(new InputStreamReader(
+			        obtain.getFile()));
+			String temp = " ";
+
+			while ((temp = list.readLine()) != null)
+			{
+				_fileName.add(temp);
 			}
-			i++ ;
 		}
-		str = (String[]) nameList.toArray() ;
-		return str ;
+		catch (IOException e)
+		{
+			System.out.println("error !");
+		}
 	}
 	
-	public status getFile(String fileName , InputStream fin) {
-			try {
-				obtain.Open(this.path , Search(fileName)[0]) ;
-			} 
-			catch (MalformedURLException e) {
-				return status.URLError ;
-			} 
-			catch (IOException e) {
-				return status.URLError ;
+	public String[] getFileList()
+	{
+		return (String[]) _fileName.toArray();
+	}
+	public String[] Search(String name)
+	{
+		ArrayList<String> nameList = new ArrayList<String>(3);
+		for (String file : _fileName)
+		{
+			if (file.indexOf(name) > 0)
+			{
+				nameList.add(file);
 			}
-			fin = obtain.getFile() ;
-			return status.done ;
+		}
+		
+		return (String[]) nameList.toArray();
+	}
+
+	public status getFile(String fileName, InputStream fin)
+	{
+		try
+		{
+			obtain.Open(this.path, Search(fileName)[0]);
+		}
+		catch (MalformedURLException e)
+		{
+			return status.URLError;
+		}
+		catch (IOException e)
+		{
+			return status.URLError;
+		}
+		fin = obtain.getFile();
+		return status.done;
 	}
 }
